0e1cd70ecd17129daa0bf826c5dd2f2441a9dfb0
sixtored <sixtod@gmail.com>
2021-09-07 22:17:39 -0300

    Demo sistema de chat online con ajax y Mysq

---
+++ container.php
@@ -0,0 +1,26 @@
+</head>
+<body class="">
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Navegacion</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="https://www.sixtored.com.ar" class="navbar-brand">SIXTORED</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Chat On Line</a></li>
+           
+          </ul>
+         
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
+	<div class=''>
+	</div>
